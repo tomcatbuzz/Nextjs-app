6c1b3323eeafbb3b2c831b7bc638442495abecd1
Tony <tomcatbuzz@yahoo.com>
2024-01-01 01:24:30 -0500

    wip text stagger

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
 import Page from "@/components/page";
+import AnimatedTextWord from "@/components/AnimatedTextWord";
 import { motion } from "framer-motion";
+import AnimatedTextCharacter from "@/components/AnimatedTextCharacter";
 
 export default function Home() {
   return (
     <Page>
+      {/* <div className="home_container">
+        <AnimatedTextWord  text="Home is here" />
+      </div> */}
+      <div className="home_container">
+        <AnimatedTextCharacter  text="Home is here" />
+      </div>
       
-      <motion.h1
+      {/* <motion.h1
       whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 0.8 }}
-      style={{ x: 100 }}>Home</motion.h1>
+      >Home</motion.h1> */}
     </Page>
   )
 }
